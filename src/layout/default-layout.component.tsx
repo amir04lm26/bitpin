f7723c87e54cfab234951a326f9fdb60ec12be22
Amir <z.amir04lm26@gmail.com>
2024-01-19 18:13:19 +0330

    fix(theme): fixes some issues

--- src/layout/default-layout.component.tsx
+++ src/layout/default-layout.component.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from "react";
 import { Header } from "../components/header";
 
-import { createTheme, ThemeProvider, Theme } from "@mui/material/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import rtlPlugin from "stylis-plugin-rtl";
 import { prefixer } from "stylis";
@@ -11,11 +11,9 @@ import createCache from "@emotion/cache";
 // Consuming the outer theme is only required with coexisting themes, like in this documentation.
 // If your app/website doesn't deal with this, you can have just:
 // const theme = createTheme({ direction: 'rtl' })
-const theme = (outerTheme: Theme) =>
-  createTheme({
-    ...outerTheme,
-    direction: "rtl",
-  });
+const theme = createTheme({
+  direction: "rtl",
+});
 
 const cacheRtl = createCache({
   key: "muirtl",
